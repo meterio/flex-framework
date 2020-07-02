7249ad4e48778da517fb4404448bd0bbbf424741
simonzg <zhanghan.simon@gmail.com>
2020-07-02 13:58:24 +0800

    add token value for explainArg

--- src/framework/explainer.ts
+++ src/framework/explainer.ts
@@ -1,60 +1,63 @@
-import { decodeRevertReason } from './revert-reason'
-import * as R from './rules'
-import * as V from 'validator-ts'
+import { decodeRevertReason } from './revert-reason';
+import * as R from './rules';
+import * as V from 'validator-ts';
 
 export function newExplainer(ctx: Context): Flex.Meter.Explainer {
-    const opts: {
-        caller?: string
-        gas?: number
-        gasPrice?: string
-    } = {}
+  const opts: {
+    caller?: string;
+    gas?: number;
+    gasPrice?: string;
+  } = {};
 
-    return {
-        caller(addr) {
-            opts.caller = R.test(addr, R.address, 'arg0').toLowerCase()
-            return this
-        },
-        gas(gas) {
-            opts.gas = R.test(gas, R.uint64, 'arg0')
-            return this
-        },
-        gasPrice(gp) {
-            opts.gasPrice = R.test(gp, R.bigInt, 'arg0').toString().toLowerCase()
-            return this
-        },
-        execute(clauses) {
-            R.test(clauses, [clauseScheme], 'arg0')
+  return {
+    caller(addr) {
+      opts.caller = R.test(addr, R.address, 'arg0').toLowerCase();
+      return this;
+    },
+    gas(gas) {
+      opts.gas = R.test(gas, R.uint64, 'arg0');
+      return this;
+    },
+    gasPrice(gp) {
+      opts.gasPrice = R.test(gp, R.bigInt, 'arg0').toString().toLowerCase();
+      return this;
+    },
+    execute(clauses) {
+      R.test(clauses, [clauseScheme], 'arg0');
 
-            const transformedClauses = clauses.map(c => {
-                return {
-                    to: c.to ? c.to.toLowerCase() : null,
-                    value: c.value.toString().toLowerCase(),
-                    data: (c.data || '0x').toLowerCase()
-                }
-            })
+      const transformedClauses = clauses.map((c) => {
+        return {
+          to: c.to ? c.to.toLowerCase() : null,
+          value: c.value.toString().toLowerCase(),
+          data: (c.data || '0x').toLowerCase(),
+          token: c.token,
+        };
+      });
 
-            return ctx.driver.explain(
-                {
-                    clauses: transformedClauses,
-                    ...opts
-                },
-                ctx.trackedHead.id)
-                .then(outputs => {
-                    return outputs.map(o => {
-                        if (o.reverted) {
-                            const revertReason = decodeRevertReason(o.data)
-                            return { ...o, decoded: { revertReason } }
-                        }
-                        return o
-                    })
-                })
-        }
-    }
+      return ctx.driver
+        .explain(
+          {
+            clauses: transformedClauses,
+            ...opts,
+          },
+          ctx.trackedHead.id
+        )
+        .then((outputs) => {
+          return outputs.map((o) => {
+            if (o.reverted) {
+              const revertReason = decodeRevertReason(o.data);
+              return { ...o, decoded: { revertReason } };
+            }
+            return o;
+          });
+        });
+    },
+  };
 }
 
 const clauseScheme: V.Scheme<Flex.Meter.Clause> = {
-    to: V.nullable(R.address),
-    token: R.uint32,
-    value: R.bigInt,
-    data: V.optional(R.bytes)
-}
+  to: V.nullable(R.address),
+  token: R.uint32,
+  value: R.bigInt,
+  data: V.optional(R.bytes),
+};
